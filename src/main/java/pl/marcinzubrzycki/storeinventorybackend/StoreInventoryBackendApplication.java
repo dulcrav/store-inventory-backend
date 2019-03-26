f43bcedb303b8fdf6cabb7eb3d2578df51d1472e
Marcin <marcinzub1@gmail.com>
2019-03-26 21:10:01 +0100

    initial commit

---
+++ src/main/java/pl/marcinzubrzycki/storeinventorybackend/StoreInventoryBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.marcinzubrzycki.storeinventorybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreInventoryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreInventoryBackendApplication.class, args);
+	}
+
+}
